484c7e79c50cf8a8dab5996aac6530d4469ddb5c
sandy-wang <sandy.wang@sungard.com>
2016-12-01 10:01:14 +1300

    Created Start-LoggingLab.ps1 in AzureAutomation.

---
+++ Start-LoggingLab.ps1
@@ -0,0 +1,14 @@
+workflow Start-LoggingLab
+{
+    Write-OutPut "This is an Output Line"
+ 
+    Write-Warning "This is a Warning Line"
+ 
+    Write-Error "This is an Error Line"
+ 
+    Write-Verbose "This is a Verbose Line"
+    
+    Write-Progress "This is a Progress Line"
+
+    Write-Debug "This is a Debug Line"
+}
